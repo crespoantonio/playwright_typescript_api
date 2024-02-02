aba6484495fc475515041189de3c35ee886be08c
crespoantonio <lic.crespoantonio@gmail.com>
2024-02-02 14:28:54 -0300

    Changes to make sure to use an existing book

--- tests/api/apiTest.spec.ts
+++ tests/api/apiTest.spec.ts
@@ -2,22 +2,30 @@ import {test, expect, APIResponse} from '@playwright/test'
 import { helpers } from '../../support/helpers';
 import { IBookingDetails } from '../../support/types';
 
-test('Create a new booking', async ({ request }) => {
-    const bookingDetails:IBookingDetails = helpers.createNewBookingDetails();
-    const response:APIResponse = await request.post("/booking", {
-        data: bookingDetails
+test.describe('/booking', ()=>{
+    let getBookingId:number
+    test.beforeAll('Get all bookings', async({request})=>{
+        const response:APIResponse = await request.get('/booking');
+        const responseBody:any = await response.json();
+        getBookingId = (responseBody[0].bookingid);
+        
+    })
+    test('Create a new booking', async ({ request }) => {
+        const bookingDetails:IBookingDetails = helpers.createNewBookingDetails();
+        const response:APIResponse = await request.post("/booking", {
+            data: bookingDetails
+        });
+        expect(response.ok()).toBeTruthy();
+        expect(response.status()).toBe(200);
+        const responseBody:any = await response.json();
+        expect(responseBody).toMatchObject(helpers.createBookingResponseValidator());
     });
-    expect(response.ok()).toBeTruthy();
-    expect(response.status()).toBe(200);
-    const responseBody:any = await response.json();
-    expect(responseBody).toMatchObject(helpers.createBookingResponseValidator());
-});
-
-test('Get booking details', async ({request}) => {
-    const getBookingId:number = 1
-    const response:APIResponse = await request.get(`/booking/${getBookingId}`)
-    expect(response.ok()).toBeTruthy();
-    expect(response.status()).toBe(200);
-    const responseBody = await response.json();
-    expect(responseBody).toMatchObject(helpers.getBookingResponseValidator());
+    
+    test('Get booking details by booking id', async ({request}) => {
+        const response:APIResponse = await request.get(`/booking/${getBookingId}`)
+        expect(response.ok()).toBeTruthy();
+        expect(response.status()).toBe(200);
+        const responseBody = await response.json();
+        expect(responseBody).toMatchObject(helpers.getBookingResponseValidator());
+    })
 })
